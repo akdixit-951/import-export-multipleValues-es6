737ec09fb5d05cf51074696ae2e2566d0899966e
akdixit-951 <142324992+akdixit-951@users.noreply.github.com>
2023-08-25 23:24:45 +0530

    exportMultipleValues.mjs

--- exportMultipleValues.mjs
+++ exportMultipleValues.mjs
@@ -1,9 +1,10 @@
-const myArray = ["camel", 265, true, "5.6"];
+export const myArray = ["camel", 265, true, "5.6"];
 
-const bulb = { watts: 10, type: "LED" };
-
-const multiplyByFour = (num) => {
-  return num * 4;
+export const bulb = {
+  watts: 10,
+  type: "LED",
 };
 
-export { myArray, bulb, multiplyByFour };
+export function multiplyByFour(num = 4) {
+  return num * 4;
+}
